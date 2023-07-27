9a65208f0da4d44120132068159360bab8ee0ec4
vrxmike <alvinvrxmike@icloud.com>
2023-07-27 22:12:24 +0000

    0-Task-Fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,7 +23,7 @@ def validUTF8(data):
             elif i >> 7 == 0b1:
                 return False
         else:
-            if i >> 6 != ob10:
+            if i >> 6 != 0b10:
                 return False
             byte_count -= 1
     return byte_count == 0
